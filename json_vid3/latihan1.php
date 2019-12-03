8be4d938ebc73bfce061a8521f0a3d3a0551775e
zikrial <zikrialhaq@mail.unpas.ac.id>
2019-12-03 09:14:08 +0700

    Mencoba json (video 3)

---
+++ json_vid3/latihan1.php
@@ -0,0 +1,13 @@
+<?php
+$mahasiswa = [
+    [
+        "nama" => "Zikri Alhaq",
+        "nrp" => "173040027",
+        "email" => "zikrialhaq@mail.unpas.ac.id"
+    ],
+    [
+        "nama" => "Adhy Wiranto",
+        "nrp" => "173040044",
+        "email" => "adhyw@mail.unpas.ac.id"
+    ]
+];
---
+++ json_vid3/script.js
@@ -0,0 +1,21 @@
+// let mahasiswa = {
+//     nama: "Zikri Alhaq",
+//     nrp: "173040027",
+//     email: "zikrialhaq@mail.unpas.ac.id"
+// }
+
+// console.log(JSON.stringify(mahasiswa));
+
+// let xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function () {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//         let mahasiswa = JSON.parse(this.responseText);
+//         console.log(mahasiswa);
+//     }
+// }
+// xhr.open('GET', 'coba.json', true);
+// xhr.send();
+
+$.getJSON('coba.json', function (data) {
+    console.log(data);
+});
